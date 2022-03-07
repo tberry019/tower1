738cb349317d7a647e58a3691f7d13e3174cd1c9
Taylor Berry <taylorberry019@gmail.com>
2022-03-07 15:01:59 -0700

    pushing heroku changes

--- tower.client/vite.config.js
+++ tower.client/vite.config.js
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   plugins: [vue()],
   build: {
-    outDir: '../Tower.client',
+    outDir: '../Tower/client',
     sourcemap: false
   },
   server: {
